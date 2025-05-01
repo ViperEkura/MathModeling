7cedb1f3e900e2027c3ce2bdc556d15d7f3a5131
ViperEkura <3081035982@qq.com>
2025-05-01 19:48:46 +0800

    修改玫瑰风向图参数

--- windroseplot.py
+++ windroseplot.py
@@ -9,18 +9,18 @@ df = pd.read_csv('processed_data.csv',
                 na_values=['NA', 'NaN', 'nan'])
 
 # 绘制图像
-fig = plt.figure(figsize=(20, 10))
+fig = plt.figure(figsize=(15, 10))
 plt.subplots_adjust(wspace=0.5, hspace=0.5)
 
 for i, feature in enumerate(features, 1):
-    ax = fig.add_subplot(2, 4, i, projection="windrose")
+    ax = fig.add_subplot(2, 3, i, projection="windrose")
     
     ax.bar(df['wd'], 
            df[feature],
            bins=8,             # 按特征值分8个区间
            cmap=plt.cm.plasma,
            edgecolor='gray')
-    
+
     ax.set_title(f"Wind with {feature}", pad=20)
     ax.set_legend(title=feature + ' Values', bbox_to_anchor=(1.1, 0.5))
 
