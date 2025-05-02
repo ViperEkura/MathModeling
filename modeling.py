b4ed787ff134c6dc0c8a695127720dacd45ae1bc
ViperEkura <3081035982@qq.com>
2025-05-02 20:01:55 +0800

    增加随机种子

--- modeling.py
+++ modeling.py
@@ -7,6 +7,20 @@ import matplotlib.pyplot as plt
 from torch import Tensor
 from torch.utils.data import Dataset, DataLoader
 from torch.optim import AdamW
+import numpy as np
+import random
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+set_seed(3304)
 
 
 class AirQualityDataset(Dataset):
