258bb4d73d7d7039846a1d65f09b7ab1b65e1616
ViperEkura <3081035982@qq.com>
2025-05-03 13:36:02 +0800

    修改规划模型

--- schedule.py
+++ schedule.py
@@ -1,17 +1,25 @@
 import pulp
 
 model = pulp.LpProblem("Minimize_Cost", pulp.LpMinimize)
-x1 = pulp.LpVariable('x1', lowBound=0, upBound=16)
-x2 = pulp.LpVariable('x2', lowBound=0, upBound=16)
-x3 = pulp.LpVariable('x3', lowBound=0, upBound=16)
+
+x1 = pulp.LpVariable('x1', lowBound=0, upBound=2, cat=pulp.LpInteger)
+x2 = pulp.LpVariable('x2', lowBound=0, upBound=3, cat=pulp.LpInteger)
+y = pulp.LpVariable('y', lowBound=0, upBound=16, cat=pulp.LpInteger)
+z1 = pulp.LpVariable('z1', lowBound=0, upBound=2, cat=pulp.LpInteger)
+z2 = pulp.LpVariable('z2', lowBound=0, upBound=2, cat=pulp.LpInteger)
+z3 = pulp.LpVariable('z3', lowBound=0, upBound=3, cat=pulp.LpInteger)
 
 
-model += 5000*x1 + 8000*x2 + 3000*x3, "Z"
+model += 5000*(x1 + x2) + 8000*y + 3000*(z1 + z2 + z3), "Total_Cost"
 
-model += 3*x1 + 5*x2 + 2*x3 >= 22.636
-model += 5000*x1 + 8000*x2 + 3000*x3 <= 1e5
+# 约束条件
+model += 3*(x1 + x2) + 5*y + 2*(z1 + z2 + z3) >= 22.12
+model += 5000*(x1 + x2) + 8000*y + 3000*(z1 + z2 + z3) <= 1e5
 
 model.solve()
-print("状态:", pulp.LpStatus[model.status])
-print("最优值 Z =", pulp.value(model.objective))
-print("最优解: x1 =", x1.value(), "x2 =", x2.value(),"x3=" ,x3.value())
+
+print("Status:", pulp.LpStatus[model.status])
+for v in model.variables():
+    print(v.name, "=", v.varValue)
+
+print("Minimum Cost =", pulp.value(model.objective))
